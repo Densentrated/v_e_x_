d9b2bb20cb3860ebed0417b764d17c54f1a720cb
D-108 <therealdensentrated+gitcommits@gmail.com>
2025-08-09 00:47:53 -0700

    created small test endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware::Logger, App, HttpServer};
+
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    println!("Starting Actix Web server on http://127.0.0.1:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .configure(routes::configure_test_routes)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
